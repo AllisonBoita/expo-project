4f6b3101950594c22564368081e7b4f06ecacbeb
Allison Cristiano Boita <allison.boita75@gmail.com>
2025-06-12 09:40:52 -0300

    feat: criando navigation

---
+++ src/types/navigation.ts
@@ -0,0 +1,11 @@
+// types/navigation.ts
+export type RootStackParamList = {
+    Login: undefined;
+    Main: undefined;
+    Form: { item?: any };
+};
+
+export type RootDrawerParamList = {
+    Home: undefined;
+};
+  
